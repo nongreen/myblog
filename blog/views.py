54cb409cf44ced83b04da34744dc7385f43e75a9
nongreen <newspaw1111@mail.ru>
2022-07-19 20:17:43 +0300

    Index was modified as View

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
+# todo: add cache
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import Post
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse('index')
+class Index(ListView):
+    template_name = 'blog/index.html'
+    # model = Post
 
+    def get_queryset_data(self):
+        post_list = Post.objects.all()
+        return post_list
+
+    def get_queryset(self):
+        post_list = self.get_queryset_data()
+        return post_list
